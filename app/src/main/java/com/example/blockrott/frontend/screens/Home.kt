6f55f979ff3cbe02b2894f6d5384b462279e5a12
rz-eizy <duartealessandroxd@gmail.com>
2025-10-07 12:58:08 -0300

    feat(ui): Implementacion AppStatistics
    Se implemento un mutableStateOf para que se pueda abrir la visualisacion del contenido de AppStatistics.

--- app/src/main/java/com/example/blockrott/frontend/screens/Home.kt
+++ app/src/main/java/com/example/blockrott/frontend/screens/Home.kt
@@ -8,15 +8,28 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.example.blockrott.frontend.components.AppStatistics
 import com.example.blockrott.frontend.components.StatisticsButton
+import com.example.blockrott.frontend.components.UsageStats
 import com.example.blockrott.frontend.theme.AppTheme
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
 
 @Composable
 fun HomeScreen(){
+    var showStatistics by remember { mutableStateOf(false) }
+    val sampleStats = listOf(
+        UsageStats("TikTok", "1h"),
+        UsageStats("Youtube", "20 min"),
+        UsageStats("Reddit", "6 min")
+    )
+    val hoursOfUse = "1h 26 min"
     Scaffold(
         topBar = {},
         bottomBar = {
@@ -30,7 +43,7 @@ fun HomeScreen(){
                     modifier = Modifier
                         .height(65.dp)
                         .width(125.dp),
-                    onClick = {/* DESPLEGAR VENTANA DE ESTADISTICAS */}
+                    onClick = {showStatistics = !showStatistics}
                 )
             }
         }
@@ -39,7 +52,9 @@ fun HomeScreen(){
             modifier = Modifier
                 .padding(innerPadding)
         ) {
-            /*CONTENIDO CENTRAL*/
+            if (showStatistics){
+                AppStatistics(hoursOfUse, sampleStats)
+            }
         }
     }
 }
