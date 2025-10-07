dcd15f1609f3c4672386b06d6d0a9d95f94aae14
rz-eizy <duartealessandroxd@gmail.com>
2025-10-07 00:25:33 -0300

    style: Configurar colores base y tema Material3
    Se configura el sistema de diseño visual de la aplicación. Se define:
    
    La paleta de colores, el esquema de color y la tipografía en los archivos Color.kt, Type.kt y Theme.kt.
    
    Esto centraliza la apariencia y prepara el proyecto para el desarrollo de los composables de UI.

---
+++ app/src/main/java/com/example/blockrott/frontend/theme/Color.kt
@@ -0,0 +1,22 @@
+package com.example.blockrott.frontend.theme
+
+import androidx.compose.ui.graphics.Color
+
+val AppBackground = Color(0xFF63C7D4)
+val OptionalAppBackground = Color(0xFF81D7E3)
+
+val PrimaryColor = Color(0xFFC7C7C7)
+val SecondaryColor = Color(0xFF7829D3)
+
+//  Colores de los componentes.
+val ComponentBackground = Color(0xFFFFFFFF)
+val ComponentSurface = Color(0xFFEBEBEB)
+
+//  Colores de Texto
+val TextPrimary = Color(0xFF000000)
+val TextSecondary = Color(0xFF4C4C4C)
+
+//  Colores Temporizadores
+val TimerBackground = Color(0xF0707070)
+val TimerTextSelected = Color(0xFFFFFFFF)
+val TimerTextUnselected = Color(0xFFADADAD)
--- app/src/main/java/com/example/blockrott/frontend/theme/Theme.kt
+++ app/src/main/java/com/example/blockrott/frontend/theme/Theme.kt
@@ -2,12 +2,34 @@ package com.example.blockrott.frontend.theme
 
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
+import androidx.compose.material3.lightColorScheme
+import androidx.compose.ui.graphics.Color
+
+private val BRColors = lightColorScheme(
+    primary = PrimaryColor,
+    onPrimary = TextPrimary,
+    secondary = SecondaryColor,
+    onSecondary = TextPrimary,
+
+
+    background = AppBackground,
+    onBackground = TextPrimary,
+    surface = ComponentBackground,
+    onSurface = TextPrimary,
+
+    error = Color.Red,
+    onError = Color.White
+)
 
 @Composable
 fun AppTheme(
     content: @Composable () -> Unit
 ) {
+    val colorScheme = BRColors
+
     MaterialTheme(
+        colorScheme = colorScheme,
+        typography = Typography,
         content = content
     )
 }
