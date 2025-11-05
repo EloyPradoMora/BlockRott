93217f220635e11b44d6d5dd3e990ffe4e6bc5e1
rz-eizy <duartealessandroxd@gmail.com>
2025-11-05 20:36:55 -0300

    feat(ui/modal): Implementa un componente BottomSheet reutilizable
    Introduce un componente `ModalBottomSheet` genérico y reutilizable para mostrar contenido temporal en la parte inferior de la pantalla, mejorando la interacción del usuario.
    
    Componente `BottomSheet`: Encapsula el `ModalBottomSheet` de Material 3, gestionando el estado de visibilidad (`showBottomSheet`) y la lógica de cierre (`onDismiss`). Está diseñado para recibir contenido composable genérico.
    
    Renombre y Centralización:
    La clase `Cards` fue renombrada a `MidContent.kt`. Este cambio centraliza la lógica y el manejo del estado (state) de los componentes ubicados en la sección central de las pantallas, alineándose con la convención arquitectónica `[Sección]Content`.

--- app/src/main/java/com/example/blockrott/frontend/components/MidContent.kt
+++ app/src/main/java/com/example/blockrott/frontend/components/MidContent.kt
@@ -6,15 +6,26 @@ import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.ColumnScope
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ElevatedCard
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.ModalBottomSheet
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -24,6 +35,9 @@ import androidx.compose.ui.unit.sp
 import com.example.blockrott.frontend.theme.AppTheme
 import com.example.blockrott.frontend.theme.ComponentBackground
 import com.example.blockrott.frontend.theme.ComponentSurface
+import com.example.blockrott.frontend.theme.SecondaryColor
+import com.example.blockrott.frontend.theme.TextSecondary
+import com.example.blockrott.frontend.theme.TimerTextSelected
 
 data class UsageStats(val appName:String, val usageTime: String)
 
@@ -90,9 +104,30 @@ fun AppStatistics(
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun BottomSheet(
+    showBottomSheet: Boolean,
+    onDismiss: () -> Unit,
+    content: @Composable ColumnScope.() -> Unit
+){
+    if (showBottomSheet) {
+        val sheetState = rememberModalBottomSheetState(
+            skipPartiallyExpanded = false
+        )
+        ModalBottomSheet(
+            onDismissRequest = onDismiss,
+            sheetState = sheetState,
+            content = content,
+            containerColor = ComponentBackground
+        )
+    }
+}
+
 @Preview(showBackground = true)
 @Composable
 fun AppPreview(){
+    /*
     val sampleStats = listOf(
         UsageStats("TikTok", "1h"),
         UsageStats("Youtube", "20 min"),
@@ -101,4 +136,13 @@ fun AppPreview(){
     AppTheme {
         AppStatistics("1h 26 min", sampleStats)
     }
+
+    HojaInferior(
+        true, {}, content = {}
+    )
+     */
+    val appsPreview = listOf(
+        "Instagram","Facebook","TikTok","Youtube","Reddit","Discord"
+    )
+    BlockConfig(appsPreview, onClickConfirm = {})
 }
