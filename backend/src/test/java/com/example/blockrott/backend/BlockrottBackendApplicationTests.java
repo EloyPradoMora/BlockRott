1c92e4d84a4a6903bb7813477fb269aca8294ad5
BastianWenckhans <basty.wenckhansbello@gmail.com>
2025-11-11 00:40:58 -0300

    feat : Prueba De Integracion
    Se empezo con el traslado de pruebas al entorno de test de srpingboot
    
    Se agrego aplication.properties a git ignore para no dejar rastro de los datos de la base de datos
    
    Se realizo la primera prueba de integracion

--- backend/src/test/java/com/example/blockrott/backend/BlockrottBackendApplicationTests.java
+++ backend/src/test/java/com/example/blockrott/backend/BlockrottBackendApplicationTests.java
@@ -1,13 +1,31 @@
 package com.example.blockrott.backend;
 
+import com.example.blockrott.backend.repositorio.RepositorioUsuario;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import java.sql.Connection;
+import java.sql.SQLException;
+import static org.junit.jupiter.api.Assertions.*;
+import javax.sql.DataSource;
 
 @SpringBootTest
 class BlockrottBackendApplicationTests {
+	@Autowired
+	private DataSource dataSource;
+
+	@Autowired
+	private RepositorioUsuario repositorioUsuario;
 
 	@Test
-	void contextLoads() {
+	void pruebaDeConexionSQL() throws SQLException {
+		System.out.println("Ejecutando prueba de conexion a la BD");
+		Connection connection = dataSource.getConnection();
+		assertNotNull(connection, "La conexion no deberian ser nula");
+		String dbName = connection.getMetaData().getDatabaseProductName();
+		assertEquals("PostgreSQL", dbName);
+		System.out.println("Conexion exitosa a: " + dbName);
+		connection.close();
 	}
 
 }
